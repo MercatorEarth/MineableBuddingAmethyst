b6a3a3f06ffe25c70b7907466cb57c6dc857c801
prabhav bhattu <prabhavbhattu@gmail.com>
2021-06-20 10:38:29 -0700

    Initial commit

---
+++ src/main/java/com/mercator/mineablebuddingamethyst/MineableBuddingAmethyst.java
@@ -0,0 +1,17 @@
+package com.mercator.mineablebuddingamethyst;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineableBuddingAmethyst extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
